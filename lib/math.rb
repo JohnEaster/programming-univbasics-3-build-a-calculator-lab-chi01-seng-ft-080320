5e102cc06a36a7684ab4f02a81145339945981c5
John Easter <john@owltestprep.com>
2020-06-25 19:07:01 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,22 +3,22 @@ def addition(num1, num2)
   puts sum
 end
 
-addition(3, 4)
-
 def subtraction(num1, num2)
-
+  difference = num1 - num2
+  puts difference
 end
 
 def division(num1, num2)
-
+  dividend = num1 / num2
 end
 
 def multiplication(num1, num2)
-
+  product = num1 * num2
+  puts product
 end
 
 def modulo(num1, num2)
-
+  modulus = num1 % num2
 end
 
 def square_root(num)
